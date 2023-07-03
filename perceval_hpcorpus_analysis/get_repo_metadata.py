911eadf8432f5cbcad04cb574959a400ebf47ff1
Niranjan Hasabnis <niranjan.hasabnis@intel.com>
2023-07-03 16:47:02 -0700

    Timestamp info of HPCorpus repos

---
+++ perceval_hpcorpus_analysis/get_repo_metadata.py
@@ -0,0 +1,10 @@
+import json
+import sys
+
+print('Processing:', sys.argv[1])
+
+f = open(sys.argv[1])
+perceval_json_data = json.load(f)
+repo_metadata = perceval_json_data['data']
+
+print(f'{repo_metadata["clone_url"]},{repo_metadata["created_at"]},{repo_metadata["updated_at"]}')
