b313a35c987d62a314008a850f45fdf2e41be6c1
Niranjan Hasabnis <niranjan.hasabnis@intel.com>
2023-07-05 10:52:57 -0700

    Adding repos with fork info now

--- perceval_hpcorpus_analysis/get_repo_metadata.py
+++ perceval_hpcorpus_analysis/get_repo_metadata.py
@@ -7,4 +7,4 @@ f = open(sys.argv[1])
 perceval_json_data = json.load(f)
 repo_metadata = perceval_json_data['data']
 
-print(f'{repo_metadata["clone_url"]},{repo_metadata["created_at"]},{repo_metadata["updated_at"]}')
+print(f'{repo_metadata["clone_url"]},{repo_metadata["created_at"]},{repo_metadata["updated_at"]},{repo_metadata["fork"]}')
